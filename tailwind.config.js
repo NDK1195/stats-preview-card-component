302ddc8df10596b77ff88d400a24961a2b40dd96
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-24 14:58:02 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
         'slightly-transparent-white-p': 'hsla(0, 0%, 100%, 0.75)',
         'slightly-transparent-white-h': 'hsla(0, 0%, 100%, 0.6)',
       },
+      boxShadow: {
+        box: '0px 20px 20px -10px rgba(23, 25, 41, 0.20)',
+      },
     },
   },
   plugins: [],
